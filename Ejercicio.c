40a8795a541f5a2eb929caf217c742007c61b5e8
Daniel <67346295+DanielFuentes12@users.noreply.github.com>
2020-08-08 21:40:41 -0300

    Add files via upload

---
+++ Ejercicio.c
@@ -0,0 +1,14 @@
+    #include<stdlib.h>
+    #include<stdio.h>
+    void main ()
+{
+    int num=0;
+    do
+    {
+        printf(" % d\n", num);
+        num=num+1;
+    }
+    while (num<10);
+    system("pause");
+
+}
